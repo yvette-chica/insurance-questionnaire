4bafc5194cd942fdf881c9d855ca5249e356dd5b
yvette <yvette@its6am.com>
2020-01-21 17:43:24 +0100

    Updates

---
+++ precache-manifest.9fbc740e5538de82b4b30cfaeef6c1c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700b0d16ba7c2921141747c806fe93ef",
+    "url": "/insurance-questionnaire/index.html"
+  },
+  {
+    "revision": "db647cde4057396c0d4d",
+    "url": "/insurance-questionnaire/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "1adab19af2c2b0218fb4",
+    "url": "/insurance-questionnaire/static/css/main.8f3af40f.chunk.css"
+  },
+  {
+    "revision": "db647cde4057396c0d4d",
+    "url": "/insurance-questionnaire/static/js/2.07acb0e2.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/insurance-questionnaire/static/js/2.07acb0e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1adab19af2c2b0218fb4",
+    "url": "/insurance-questionnaire/static/js/main.399d0e32.chunk.js"
+  },
+  {
+    "revision": "b6e4270e767930d0da9e",
+    "url": "/insurance-questionnaire/static/js/runtime-main.d21c13d3.js"
+  }
+]);
